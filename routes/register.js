d311d963154bd851bed9086aba6bda4b06db1220
Collin Smith <collin.smith@iostudio.com>
2020-01-03 00:00:47 -0600

    storing users in the database and more

--- routes/register.js
+++ routes/register.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const { db } = require("../mysql");
 
 const random = require("randomstring");
+const bcrypt = require("bcrypt");
 
 const dotenv = require("dotenv").config();
 const nodemailer = require("nodemailer");
@@ -23,15 +25,29 @@ router.get("/", (req, res, next) => {
 
 // '/register/verify' router — register form submission
 router.post("/verify", (req, res, next) => {
-  const to = req.body.email;
+  const userEmail = req.body["email"];
+  const userPass = req.body["new-password"];
+  // generate a random 6 digit Verification code
   const code = random.generate({
     length: 6,
     charset: "numeric"
   });
 
+  // encrypt user password and store user data with auth code
+  bcrypt.hash(userPass, 10, function(err, hash) {
+    if (err) throw err;
+    db.query(
+      `insert into users (email, password, auth_code) values ('${userEmail}', '${hash}', '${code}')`,
+      (err, result) => {
+        if (err) throw err;
+      }
+    );
+  });
+
+  // create mail object and send email using nodemailer
   const mail = {
     from: user,
-    to,
+    to: userEmail,
     subject: "Tisk Task Account Verification",
     text: code
   };
